e841f91e4a6ef222e937204e8ccc7f1726d53fc1
Choirul Huda <121425666+cehuda1@users.noreply.github.com>
2024-02-22 10:26:59 +0700

    Create kid.js

---
+++ kid.js
@@ -0,0 +1,24 @@
+document.head.innerHTML = `<style>* {
+  margin: 0;
+  padding: 0;
+}
+.hacked {
+  background: black;
+  color: lime;
+  height: 100vh;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  font-size: 3rem;
+}</style>`;
+
+document.body.innerHTML = `
+<div class="yahahaha">
+  <h1>Hemker disini</h1>
+</div>
+`;
+
+window.addEventListener('click', e => alert('yahahahha hemker ni bwos'));
